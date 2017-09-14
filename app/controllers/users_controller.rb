fc48a766e10c19889ba8afc171d07b39a20f5430
Hui Qian Liang <liang.huiqian@gmail.com>
2017-09-14 11:57:11 +0800

    Added sign up form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/show'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def login
+  end
+
+  def logout
+    session.clear
+    redirect_to 'welcome/index'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :location)
+  end
+
 end
